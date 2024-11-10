57bc307c4edf93eefa9dbd676a1306d2bfeb2192
Logan Kriebel <logan.kriebel@gmail.com>
2024-11-10 05:57:26 -0500

    made scroll.h usable

--- src/test/scroll.h
+++ src/test/scroll.h
@@ -1,6 +1,7 @@
 #ifndef SCROLL_H
 #define SCROLL_H
 
+#include <curses.h>
 struct scroll_text {
     char *message;
     int n;
@@ -13,5 +14,9 @@ struct scroll_text {
 /* text is always bound to window: to create text you must also define
  * the window it is a part of
  */
+struct scroll_text create_text(WINDOW *win, char *message, int y);
+int on_border(WINDOW *win, struct scroll_text e);
+void scroll_update(WINDOW *win, struct scroll_text *e);
+void paste_scroll_text(WINDOW *win, int y, int x, char *message);
 
 #endif /* SCROLL_H */
